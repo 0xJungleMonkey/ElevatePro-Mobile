0585968fb32c06c8119969302ed87b718d20c253
Monkey 0xJungle.com <Monkey@0xJungle.com>
2022-12-28 15:06:40 -0500

    Beautiful_Useful_login

--- Login.js
+++ Login.js
@@ -4,115 +4,121 @@ import React, { useState } from "react";
 import { Button, TextInput, Text, Card, List } from "react-native-paper";
 import { View, Image, StyleSheet } from "react-native";
 import { useTheme } from "react-native-paper";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 export default function LoginScreen({ navigation }) {
   const theme = useTheme();
 
   const [email, setEmail] = useState("");
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <View style={styles.top_bar}>
-        <Text
+    <KeyboardAwareScrollView>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <View style={styles.newContainer}>
+          <Image style={styles.image} source={require("./assets/logo.jpg")} />
+        </View>
+        <View style={styles.top_bar}>
+          <Text
+            style={{
+              position: "relative",
+              fontSize: 28,
+              paddingLeft: "3%",
+              paddingRight: "3%",
+              textAlign: "center",
+              color: theme.colors.primary,
+            }}
+          >
+            Enter your mail to get started.
+          </Text>
+        </View>
+        <View
           style={{
+            paddingLeft: "2%",
+            paddingRight: "2%",
+          }}
+        >
+          <Text
+            style={{
+              color: theme.colors.onBackground,
+              fontSize: 16,
+              textAlign: "center",
+              paddingBottom: "4%",
+              opacity: 0.4,
+            }}
+          >
+            {`This app for internal use only. You will need to use work email.`}
+          </Text>
+        </View>
+
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            label="Email"
+            mode="outlined "
+            placeholder="Type something"
+            placeholderTextColor="#003f5c"
+            onChangeText={(email) => setEmail(email)}
+            right={<TextInput.Affix text="/100" />}
+          />
+        </View>
+
+        <View>
+          <Button
+            style={styles.loginBtn}
+            mode="contained"
+            onPress={() => navigation.navigate("Home")}
+          >
+            LOGIN
+          </Button>
+        </View>
+
+        <View
+          style={{
+            paddingLeft: "2%",
+            paddingRight: "2%",
             position: "relative",
-            fontSize: 28,
-            paddingLeft: "3%",
-            paddingRight: "3%",
-            textAlign: "center",
-            color: theme.colors.primary,
           }}
         >
-          Enter your mail to get started.
-        </Text>
-      </View>
-      <View
-        style={{
-          paddingLeft: "2%",
-          paddingRight: "2%",
-        }}
-      >
-        <Text
+          <Text
+            style={{
+              color: theme.colors.secondary,
+              fontSize: 14,
+              opacity: 0.4,
+              marginLeft: 20,
+            }}
+          >
+            By signing up.
+          </Text>
+        </View>
+        <View
           style={{
-            color: theme.colors.onBackground,
-            fontSize: 16,
-            textAlign: "center",
-            paddingBottom: "4%",
-            opacity: 0.4,
+            flexDirection: "row",
+            position: "relative",
+            paddingLeft: "2%",
+            paddingRight: "2%",
           }}
         >
-          {`This app for internal use only. You will need to use work email.`}
-        </Text>
+          <Text
+            style={{
+              color: theme.colors.secondary,
+              fontSize: 14,
+              opacity: 0.4,
+              marginLeft: 20,
+            }}
+          >
+            {"You agree to the "}
+          </Text>
+          <Text
+            style={{
+              color: theme.colors.secondary,
+              fontSize: 15,
+              marginRight: 20,
+            }}
+          >
+            {"Terms & Conditions"}
+          </Text>
+        </View>
       </View>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          label="Email"
-          mode="outlined "
-          placeholder="Type something"
-          placeholderTextColor="#003f5c"
-          onChangeText={(email) => setEmail(email)}
-          right={<TextInput.Affix text="/100" />}
-        />
-      </View>
-      <View>
-        <Button
-          style={styles.loginBtn}
-          mode="contained"
-          onPress={() => navigation.navigate("Home")}
-        >
-          LOGIN
-        </Button>
-      </View>
-      <View style={styles.newContainer}>
-        <Image style={styles.image} source={require("./assets/logo.jpg")} />
-      </View>
-      <View
-        style={{
-          paddingLeft: "2%",
-          paddingRight: "2%",
-          position: "relative",
-        }}
-      >
-        <Text
-          style={{
-            color: theme.colors.secondary,
-            fontSize: 14,
-            opacity: 0.4,
-            marginLeft: 20,
-          }}
-        >
-          By signing up.
-        </Text>
-      </View>
-      <View
-        style={{
-          flexDirection: "row",
-          position: "relative",
-          paddingLeft: "2%",
-          paddingRight: "2%",
-        }}
-      >
-        <Text
-          style={{
-            color: theme.colors.secondary,
-            fontSize: 14,
-            opacity: 0.4,
-            marginLeft: 20,
-          }}
-        >
-          {"You agree to the "}
-        </Text>
-        <Text
-          style={{
-            color: theme.colors.secondary,
-            fontSize: 15,
-            marginRight: 20,
-          }}
-        >
-          {"Terms & Conditions"}
-        </Text>
-      </View>
-    </View>
+    </KeyboardAwareScrollView>
   );
 }
 
