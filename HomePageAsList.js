7aa40538011be90a3ca58f59065730bfeb02057d
Monkey 0xJungle.com <Monkey@0xJungle.com>
2023-01-02 02:21:42 -0500

    Home.js optimized version.expandable list

--- HomePageAsList.js
+++ HomePageAsList.js
@@ -2,24 +2,23 @@
 //Under the section in charge of displaying the list, a "Log Out" button redirects the user to the login screen.
 
 <script src="http://localhost:8097"></script>;
-import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { ScrollView, TouchableOpacity, View } from "react-native";
+
 import { useTheme, Title, List } from "react-native-paper";
 
 //The HomeScreen component is a functional component that displays a touchable card
 // touchable card containing the title and content variables. When the
 // touchable area is pressed, it uses the navigation prop to navigate to the
 // "Details" screen and pass along the title and content as parameters.
-function GetAllCards(props) {
+function GetAllCards() {
   const theme = useTheme();
   const [data, setData] = useState([{ id: "1", status: "500" }]);
-
   useEffect(() => {
     axios
-      .get("http://localhost:5287/api/elevator/intervention")
-      .then((response) => setData(response.data))
+      .get("http://localhost:8080/elevators_?page=1&pagesize=100&order=id")
+      .then((response) => setData(response.data.data))
       .catch((error) => console.log(error));
   }, []);
   return (
@@ -28,7 +27,7 @@ function GetAllCards(props) {
         return (
           <TouchableOpacity
             onPress={() =>
-              props.navigation?.push("Details", {
+              navigation?.push("Details", {
                 item,
                 i,
               })
@@ -49,30 +48,13 @@ function GetAllCards(props) {
     </View>
   );
 }
-export default HomeScreen = ({ navigation }) => {
-  const [expanded, setExpanded] = React.useState(true);
-  const theme = useTheme();
-
-  const handlePress = () => setExpanded(!expanded);
+export default HomeScreen = () => {
   return (
     <View>
       <ScrollView>
         <List.Section>
-          <List.Accordion
-            title="Intervention Elevators"
-            left={() => (
-              <List.Icon color={theme.colors.primary} icon="elevator" />
-            )}
-            id="1"
-            expanded={expanded}
-            onPress={handlePress}
-          >
-            <GetAllCards navigation={navigation} />
-          </List.Accordion>
+          <GetAllCards />
         </List.Section>
-        {/* 
-          <List.Subheader>Intervention Elevators</List.Subheader>
-        */}
       </ScrollView>
     </View>
   );
